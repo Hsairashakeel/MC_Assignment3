f3cf4a3b1878e4f0a000e5e74d3fc5bc5c2d4d21
hsairashakeel <sairashakeel.py@gmail.com>
2020-04-27 05:16:41 -0400

    layout

---
+++ Assign3/app/src/main/java/com/example/assign3/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.assign3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //RecyclerView rv= (RecyclerView) findViewById(R.id.rec_view);
+        //LinearLayoutManager llm=new LinearLayoutManager(this);
+//        rv.setLayoutManager(llm);
+//        String [] t={"title1","title2","title3","title4","title5","title6","title7","title8","title9","title10","title11"};
+//        String [] sub={"title1","title2","title3","title4","title5","title6","title7","title8","title9","title10","title11"};
+//        Integer [] img={R.drawable.minion,R.drawable.m2};
+//        RVAdapter adp=new RVAdapter(this,t,sub,img);
+//        rv.setAdapter(adp);
+
+
+
+    }
+}
