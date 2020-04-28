b0e31c46b5aa7fcf4d1e416b62c858915c12d670
hsairashakeel <sairashakeel.py@gmail.com>
2020-04-28 14:45:27 -0400

    recyclerView

--- Assign3/app/src/main/java/com/example/assign3/MainActivity.java
+++ Assign3/app/src/main/java/com/example/assign3/MainActivity.java
@@ -1,27 +1,67 @@
 package com.example.assign3;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> title = new ArrayList<>();
+    ArrayList<String> level = new ArrayList<>();
+    ArrayList<String> info = new ArrayList<>();
+    ArrayList<String> image= new ArrayList<>();
+    ArrayList<String> btnClick= new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //RecyclerView rv= (RecyclerView) findViewById(R.id.rec_view);
-        //LinearLayoutManager llm=new LinearLayoutManager(this);
-//        rv.setLayoutManager(llm);
-//        String [] t={"title1","title2","title3","title4","title5","title6","title7","title8","title9","title10","title11"};
-//        String [] sub={"title1","title2","title3","title4","title5","title6","title7","title8","title9","title10","title11"};
-//        Integer [] img={R.drawable.minion,R.drawable.m2};
-//        RVAdapter adp=new RVAdapter(this,t,sub,img);
-//        rv.setAdapter(adp);
+        RecyclerView rv= (RecyclerView) findViewById(R.id.rec_view);
+        LinearLayoutManager llm=new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+        try {
+            JSONObject obj = new JSONObject(getJSONobject());
+            JSONArray bookData = obj.getJSONArray("books");
+            for (int i = 0; i < bookData.length(); i++) {
+                JSONObject bookDetail = bookData.getJSONObject(i);
+                title.add(bookDetail.getString("title"));
+                level.add(bookDetail.getString("level"));
+                info.add(bookDetail.getString("info"));
+                image.add(bookDetail.getString("cover"));
+                btnClick.add(bookDetail.getString("url"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
+//        RVAdapter adapter = new RVAdapter(this, title, level,info);
+//
+//        rv.setAdapter(adapter);
+    }
+    public String getJSONobject() {
+        String jsonData = "";
+        try {
+
+            InputStream is = getResources().openRawResource(R.raw.issues);
+            byte[] data = new byte[is.available()];
+            while (is.read(data)!=-1){
+            }
+            jsonData = new String(data);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return jsonData;
+    }
 
 
     }
-}
+
--- Assign3/app/src/main/java/com/example/assign3/RVAdapter.java
+++ Assign3/app/src/main/java/com/example/assign3/RVAdapter.java
@@ -1,75 +1,107 @@
-//package com.example.assign3;
-//
-//
-//import android.content.Context;
-//import android.util.Log;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//import android.widget.Toast;
-//
-//import androidx.annotation.NonNull;
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
-//    Context context;
-//
-//    private String[] maintitle;
-//    private String[] subtitle;
-//    private Integer[] imgid;
-//
-//    public RVAdapter(Context context, String[] maintitle, String[] subtitle, Integer[] imgid) {
-//        this.context = context;
-//        this.maintitle = maintitle;
-//        this.subtitle = subtitle;
-//        this.imgid = imgid;
-//    }
-//
-//
-//    @NonNull
-//    @Override
-//    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        View row = LayoutInflater.from(context).inflate(R.layout.list_items, null);
-//        ViewHolder holder = new ViewHolder(row);
-//        return holder;
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
-//        holder.tv_title.setText(maintitle[position]);
-//        holder.tv_subtitle.setText(subtitle[position]);
-//        holder.iv_icon.setImageResource((position % 2 == 1) ? imgid[0] : imgid[1]);
-//
-//
-//        Log.e("title at pos-"+position,maintitle[position]);
-//
-//
-//        holder.iv_icon.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(context, maintitle[position], Toast.LENGTH_LONG).show();
-//            }
-//        });
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return maintitle.length;
-//    }
-//
-//    public class ViewHolder extends RecyclerView.ViewHolder {
-//        public TextView tv_title;
-//        public TextView tv_subtitle;
-//        public ImageView iv_icon;
-//
-//        public ViewHolder(@NonNull View itemView) {
-//            super(itemView);
-//
-//            tv_title = itemView.findViewById(R.id.title);
-//            tv_subtitle = itemView.findViewById(R.id.subtitle);
-//            iv_icon = itemView.findViewById(R.id.imageView);
-//        }
-//    }
-//}
+package com.example.assign3;
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import android.view.LayoutInflater;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import android.view.View;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
+    Context context;
+
+    private ArrayList<String> title;
+    private ArrayList<String> level;
+    private ArrayList<String> info;
+    private ArrayList<String> imgid;
+    private ArrayList<String> bookBtn;
+    public RVAdapter(Context context, ArrayList<String> title, ArrayList<String> level, ArrayList<String> info, ArrayList<String> imgid, ArrayList<String> bookUrl) {
+
+        this.context = context;
+
+        this.imgid = new ArrayList<String>(imgid.size());
+        for (String i : imgid) {
+            this.imgid.add(new String(i));
+        }
+        this.title = new ArrayList<String>(title.size());
+        for (String i : title) {
+
+            this.title.add(new String(i));
+        }
+        this.level = new ArrayList<String>(level.size());
+        for (String i : level) {
+            this.level.add(new String(i));
+        }
+        this.info = new ArrayList<String>(info.size());
+        for (String i : info) {
+
+            this.info.add(new String(i));
+        }
+
+
+        this.bookBtn = new ArrayList<String>(bookUrl.size());
+        for (String i : bookUrl) {
+            this.bookBtn.add(new String(i));
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View row = LayoutInflater.from(context).inflate(R.layout.list_items, null);
+        ViewHolder holder = new ViewHolder(row);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        holder.tv_title.setText(title.get(position));
+        holder.tv_level.setText(level.get(position));
+        holder.tv_info.setText(info.get(position));
+        Picasso.with(context).load("https://raw.githubusercontent.com/revolunet/PythonBooks/master/" + imgid.get(position)).into(holder.image);
+        String a = bookBtn.get(position);
+        String asub = a.substring(a.length() - 3, a.length());
+        if (asub.equals("zip") || asub.equals("pdf")) {
+            holder.btn_book.setText("DOWNLOAD");
+        } else {
+            holder.btn_book.setText("READ ONLINE");
+        }
+
+
+    }
+    @Override
+    public int getItemCount() {
+        return title.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView tv_title;
+        public TextView tv_level;
+        public TextView tv_info;
+        public ImageView image;
+        public Button btn_book;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tv_title = itemView.findViewById(R.id.title);
+            tv_level = itemView.findViewById(R.id.level);
+            tv_info = itemView.findViewById(R.id.info);
+            image = itemView.findViewById(R.id.imageView);
+            btn_book = itemView.findViewById(R.id.button);
+        }
+    }
+
+}
+
+
+
+
+
